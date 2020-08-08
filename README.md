9d61af8bd002e0683853bfb062c3c0547ee2941b
zhiyong <1585855343@qq.com>
2020-08-08 09:58:01 +0800

    Java 二分查找法的实现

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # java_pro_01
 学习 Java 的基础，做一些小项目练手
+
+
+# dataStructureAlgorithm
+学习 Java 数据结构和算法
+    1. ch2       数组
+
---
+++ dataStructureAlgorithm/ch2/OrderdApp.java
@@ -0,0 +1,103 @@
+/*
+*   二分法查找：
+*
+*       插入的数组已经有序排列
+*       
+*   
+*/
+
+
+public class OrderdApp {
+    public static void main(String[] args){
+        
+        int maxSize = 100;
+        OrdArray arr;
+        arr = new OrdArray(maxSize);
+
+        arr.insert(17);
+        arr.display();
+
+    }
+}
+
+
+class OrdArray {
+    // elements
+    private long[] a;
+    private int nElems;
+
+    // methods
+    // init class
+    public OrdArray(int max)
+    {
+        a = new long[max];
+        nElems = 0;
+    }
+
+    public int size()
+    {
+        return nElems;
+    }
+
+    public int find(long searchKey)
+    {
+        int lowerBound = 0;
+        int upperBound = nElems - 1;
+        int curIn;
+
+        while(true)
+        {
+            curIn = (lowerBound + upperBound) / 2;
+
+            if (a[curIn] == searchKey)
+                return curIn;           // found it
+            else if (lowerBound > upperBound)
+                return nElems;          // can't find it
+            else 
+            {
+                if (a[curIn] < searchKey)
+                    lowerBound = curIn + 1;
+                else
+                    upperBound = curIn - 1;
+            }
+        }
+    }
+
+    public void insert(long value)
+    {
+        int j;
+        
+        for (j = 0; j < nElems; j++)
+            if (a[j] > value)
+                break;
+        for (int k = nElems; k > j; k--)
+            a[k] = a[k-1];
+        
+        a[j] = value;
+        nElems++;
+    }
+
+    public boolean delete(long value)
+    {
+        int j = find(value);
+
+        if (j == nElems)
+            return false;
+        else
+        {
+            for (int k = j; k < nElems; k++)
+                a[k] = a[k+1];
+            nElems--;
+            return true;
+        }
+    }
+
+    public void display()
+    {
+        for (int j = 0; j < nElems; j++)
+            System.out.print(a[j] + "  ");
+        System.out.println("");
+    }
+
+}
+
